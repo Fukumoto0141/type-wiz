1449b26fe96e36210a38ae7712b40d7acd7ab1e9
Fukumoto0141 <fukukou1256@gmail.com>
2026-02-06 02:17:03 +0900

    リポジトリ変更

--- src/update.rs
+++ src/update.rs
@@ -3,7 +3,7 @@ use self_update::cargo_crate_version;
 pub fn update() -> Result<self_update::Status, Box<dyn std::error::Error>> {
     let status = self_update::backends::github::Update::configure()
         .repo_owner("Fukumoto0141")
-        .repo_name("type-wiz-dev")
+        .repo_name("type-wiz")
         .bin_name("typewiz")
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
